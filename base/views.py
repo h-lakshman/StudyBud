137b01f2c7be89c9bf85b4f1e86187c7195779cf
h-lakshman <Lakshman.vaibhav007@gmail.com>
2023-07-08 22:16:54 +0530

    Created a new project StudyBud where all the learers join and create a room for them and message in the room.It acts just like a discord

---
+++ base/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Room
+
+def home(request):
+    rooms = Room.objects.all()
+    return render(request, 'base/home.html', {'rooms': rooms})
+
+
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    return render(request, 'base/room.html', {'room':room})
